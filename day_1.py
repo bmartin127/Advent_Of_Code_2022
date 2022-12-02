9b597e1edd54e1c7839f785c779a9cb239bcd0c7
Jeff Martin <jmartin127@gmail.com>
2022-12-01 19:28:47 -0700

    ready_for_advent

--- day_1.py
+++ day_1.py
@@ -9,5 +9,31 @@ readin_list = readin.read()
 # replacing end splitting the text 
 # when newline ('\n') is seen.
 data_into_list = readin_list.split("\n")
-print(data_into_list)
+# 
+# print(data_into_list)
 readin.close()
+
+
+readin_array = np.genfromtxt("readin.txt", dtype=str,
+                     encoding=None, delimiter="")
+# print(readin_array)
+added = int(0)
+answer = int(0)
+calories = []
+for i in data_into_list:
+    if i == (""):
+        calories.append(added)
+
+
+        if added > (answer ):
+            answer = added
+        added = 0
+    else:
+        added = added + int(i)
+print("answer is")
+calories.sort()
+finanswer = calories[len(calories) - 1] + calories[len(calories) - 2] + calories[len(calories) - 3]
+print (finanswer)
+
+
+
