23a2d5171bc9e9252014944606b2b9c11913b5bc
Jeff Martin <jmartin127@gmail.com>
2022-12-03 07:57:29 -0700

    pt-4-added

---
+++ day_4_pt-1.py
@@ -0,0 +1,13 @@
+
+
+# opening the file in read mode
+readin = open("readin.txt", "r")
+  
+# reading the file
+readin_list = readin.read()
+  
+# replacing end splitting the text 
+# when newline ('\n') is seen.
+data_into_list = readin_list.split("\n")
+# print(data_into_list)
+
